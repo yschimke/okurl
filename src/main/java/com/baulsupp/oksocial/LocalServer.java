59999f6fc3a9f4ea45680405faf45a8c91f53980
Yuri Schimke <yuri@schimke.ee>
2016-01-30 10:47:33 -0800

    reformat

--- src/main/java/com/baulsupp/oksocial/LocalServer.java
+++ src/main/java/com/baulsupp/oksocial/LocalServer.java
@@ -16,175 +16,176 @@ package com.baulsupp.oksocial;
 
 import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.google.api.client.util.Throwables;
-
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Request;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.AbstractHandler;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.Request;
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.AbstractHandler;
 
 /**
  * OAuth 2.0 verification code receiver that runs a Jetty server on a free port, waiting for a
  * redirect with the verification code.
  *
- * <p>
- * Implementation is thread-safe.
- * </p>
+ * <p> Implementation is thread-safe. </p>
  *
- * @since 1.11
  * @author Yaniv Inbar
+ * @since 1.11
  */
 public final class LocalServer implements VerificationCodeReceiver {
-    /** Server or {@code null} before {@link #getRedirectUri()}. */
-    private Server server;
+  /** Server or {@code null} before {@link #getRedirectUri()}. */
+  private Server server;
 
-    /** Verification code or {@code null} for none. */
-    String code;
+  /** Verification code or {@code null} for none. */
+  String code;
 
-    /** Error code or {@code null} for none. */
-    String error;
+  /** Error code or {@code null} for none. */
+  String error;
 
-    /** Lock on the code and error. */
-    final Lock lock = new ReentrantLock();
+  /** Lock on the code and error. */
+  final Lock lock = new ReentrantLock();
 
-    /** Condition for receiving an authorization response. */
-    final Condition gotAuthorizationResponse = lock.newCondition();
+  /** Condition for receiving an authorization response. */
+  final Condition gotAuthorizationResponse = lock.newCondition();
 
-    /** Port to use or {@code -1} to select an unused port in {@link #getRedirectUri()}. */
-    private int port;
+  /** Port to use or {@code -1} to select an unused port in {@link #getRedirectUri()}. */
+  private int port;
 
-    /** Host name to use. */
-    private final String host;
+  /** Host name to use. */
+  private final String host;
 
-    public LocalServer() {
-        this("localhost", -1);
+  public LocalServer() {
+    this("localhost", -1);
+  }
+
+  /**
+   * Constructor.
+   *
+   * @param host Host name to use
+   * @param port Port to use or {@code -1} to select an unused port
+   */
+  public LocalServer(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+
+  @Override
+  public String getRedirectUri() throws IOException {
+    if (port == -1) {
+      port = getUnusedPort();
     }
-
-    /**
-     * Constructor.
-     *
-     * @param host Host name to use
-     * @param port Port to use or {@code -1} to select an unused port
-     */
-    public LocalServer(String host, int port) {
-        this.host = host;
-        this.port = port;
+    server = new Server(port);
+    for (Connector c : server.getConnectors()) {
+      c.setHost(host);
     }
+    server.addHandler(new CallbackHandler());
+    try {
+      server.start();
+    } catch (Exception e) {
+      Throwables.propagateIfPossible(e);
+      throw new IOException(e);
+    }
+    return "http://" + host + ":" + port + "/callback";
+  }
+
+  @Override
+  public String waitForCode() throws IOException {
+    lock.lock();
+    try {
+      while (code == null && error == null) {
+        gotAuthorizationResponse.awaitUninterruptibly();
+      }
+      if (error != null) {
+        throw new IOException("User authorization failed (" + error + ")");
+      }
+      return code;
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  @Override
+  public void stop() throws IOException {
+    if (server != null) {
+      try {
+        server.stop();
+      } catch (Exception e) {
+        Throwables.propagateIfPossible(e);
+        throw new IOException(e);
+      }
+      server = null;
+    }
+  }
+
+  /**
+   * Returns the host name to use.
+   *
+   * @return host name
+   */
+  public String getHost() {
+    return host;
+  }
+
+  /**
+   * Returns the port to use or {@code -1} to select an unused port in {@link #getRedirectUri()}.
+   *
+   * @return specific port or -1
+   */
+  public int getPort() {
+    return port;
+  }
+
+  private static int getUnusedPort() throws IOException {
+    Socket s = new Socket();
+    s.bind(null);
+    try {
+      return s.getLocalPort();
+    } finally {
+      s.close();
+    }
+  }
+
+  /**
+   * Jetty handler that takes the verifier token passed over from the OAuth provider and stashes it
+   * where {@link #waitForCode} will find it.
+   */
+  class CallbackHandler extends AbstractHandler {
 
     @Override
-    public String getRedirectUri() throws IOException {
-        if (port == -1) {
-            port = getUnusedPort();
-        }
-        server = new Server(port);
-        for (Connector c : server.getConnectors()) {
-            c.setHost(host);
-        }
-        server.addHandler(new CallbackHandler());
-        try {
-            server.start();
-        } catch (Exception e) {
-            Throwables.propagateIfPossible(e);
-            throw new IOException(e);
-        }
-        return "http://" + host + ":" + port + "/callback";
+    public void handle(
+        String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
+        throws IOException {
+      writeLandingHtml(response);
+      response.flushBuffer();
+      ((Request) request).setHandled(true);
+      lock.lock();
+      try {
+        error = request.getParameter("error");
+        code = request.getParameter("code");
+        gotAuthorizationResponse.signal();
+      } finally {
+        lock.unlock();
+      }
     }
 
-    @Override
-    public String waitForCode() throws IOException {
-        lock.lock();
-        try {
-            while (code == null && error == null) {
-                gotAuthorizationResponse.awaitUninterruptibly();
-            }
-            if (error != null) {
-                throw new IOException("User authorization failed (" + error + ")");
-            }
-            return code;
-        } finally {
-            lock.unlock();
-        }
+    private void writeLandingHtml(HttpServletResponse response) throws IOException {
+      response.setStatus(HttpServletResponse.SC_OK);
+      response.setContentType("text/html");
+
+      PrintWriter doc = response.getWriter();
+      doc.println("<html>");
+      doc.println("<head><title>Authentication Token Received</title></head>");
+      doc.println("<body>");
+      doc.println("Received verification code. You may now close this window...");
+      doc.println("</body>");
+      doc.println("</HTML>");
+      doc.flush();
     }
-
-    @Override
-    public void stop() throws IOException {
-        if (server != null) {
-            try {
-                server.stop();
-            } catch (Exception e) {
-                Throwables.propagateIfPossible(e);
-                throw new IOException(e);
-            }
-            server = null;
-        }
-    }
-
-    /** Returns the host name to use. */
-    public String getHost() {
-        return host;
-    }
-
-    /**
-     * Returns the port to use or {@code -1} to select an unused port in {@link #getRedirectUri()}.
-     */
-    public int getPort() {
-        return port;
-    }
-
-    private static int getUnusedPort() throws IOException {
-        Socket s = new Socket();
-        s.bind(null);
-        try {
-            return s.getLocalPort();
-        } finally {
-            s.close();
-        }
-    }
-
-    /**
-     * Jetty handler that takes the verifier token passed over from the OAuth provider and stashes it
-     * where {@link #waitForCode} will find it.
-     */
-    class CallbackHandler extends AbstractHandler {
-
-        @Override
-        public void handle(
-                String target, HttpServletRequest request, HttpServletResponse response, int dispatch)
-                throws IOException {
-            writeLandingHtml(response);
-            response.flushBuffer();
-            ((Request) request).setHandled(true);
-            lock.lock();
-            try {
-                error = request.getParameter("error");
-                code = request.getParameter("code");
-                gotAuthorizationResponse.signal();
-            } finally {
-                lock.unlock();
-            }
-        }
-
-        private void writeLandingHtml(HttpServletResponse response) throws IOException {
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setContentType("text/html");
-
-            PrintWriter doc = response.getWriter();
-            doc.println("<html>");
-            doc.println("<head><title>Authentication Token Received</title></head>");
-            doc.println("<body>");
-            doc.println("Received verification code. You may now close this window...");
-            doc.println("</body>");
-            doc.println("</HTML>");
-            doc.flush();
-        }
-    }
+  }
 }
