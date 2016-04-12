3ccc12c1f690826282d9b037443a8011a2e41bc1
Yuri Schimke <yuri@schimke.ee>
2016-04-11 21:30:16 -0700

    auth test

--- src/main/java/com/baulsupp/oksocial/twitter/TwitterAuthInterceptor.java
+++ src/main/java/com/baulsupp/oksocial/twitter/TwitterAuthInterceptor.java
@@ -69,16 +69,16 @@ public class TwitterAuthInterceptor implements Interceptor {
     return "\"" + str + "\"";
   }
 
-  private long generateTimestamp() {
+  public long generateTimestamp() {
     long timestamp = System.currentTimeMillis();
     return timestamp / 1000;
   }
 
-  private String generateNonce() {
+  public String generateNonce() {
     return Long.toString(Math.abs(secureRandom.nextLong())) + System.currentTimeMillis();
   }
 
-  private String generateAuthorization(Request request)
+  public String generateAuthorization(Request request)
       throws NoSuchAlgorithmException, InvalidKeyException, IOException {
     long timestampSecs = generateTimestamp();
     String nonce = generateNonce();
---
+++ src/test/java/okhttp/twitter/TwitterAuthInterceptorTest.java
@@ -0,0 +1,51 @@
+package okhttp.twitter;
+
+import com.baulsupp.oksocial.twitter.TwitterAuthInterceptor;
+import com.baulsupp.oksocial.twitter.TwitterCredentials;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import okhttp3.MediaType;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TwitterAuthInterceptorTest {
+  class FixedTimeTwitterAuthInterceptor extends TwitterAuthInterceptor {
+    public FixedTimeTwitterAuthInterceptor(
+        TwitterCredentials credentials) {
+      super(credentials);
+    }
+
+    @Override public long generateTimestamp() {
+      return 1460432867L;
+    }
+
+    @Override public String generateNonce() {
+      return "67822045175727268931460435281112";
+    }
+  }
+
+  @Test
+  public void testInitialRequestAuth()
+      throws NoSuchAlgorithmException, InvalidKeyException, IOException {
+    RequestBody body = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"),
+        "oauth_callback=oob");
+    Request request =
+        new Request.Builder().url("https://api.twitter.com/oauth/request_token").post(body).build();
+    String header =
+        new FixedTimeTwitterAuthInterceptor(
+            TwitterAuthInterceptor.TEST_CREDENTIALS).generateAuthorization(
+            request);
+
+    assertEquals(
+        "OAuth oauth_consumer_key=\"pKrYKZjbhN7rmtWXenRgr8kHY\", "
+            + "oauth_nonce=\"67822045175727268931460435281112\", "
+            + "oauth_signature=\"c8fLuVd%2FkkePEmMg%2Bo5k4jNESMQ%3D\", "
+            + "oauth_signature_method=\"HMAC-SHA1\", "
+            + "oauth_timestamp=\"1460432867\", oauth_version=\"1.0\"",
+        header);
+  }
+}
