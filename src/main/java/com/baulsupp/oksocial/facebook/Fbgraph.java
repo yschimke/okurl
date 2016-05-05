3358d76d1b1bff5e314ec4306e85bf4827a1adaf
Yuri Schimke <yuri@schimke.ee>
2016-05-05 12:42:27 +1000

    remove any direct fb commands from core

--- src/main/java/com/baulsupp/oksocial/facebook/Fbgraph.java
+++
@@ -1,9 +0,0 @@
-package com.baulsupp.oksocial.facebook;
-
-import com.baulsupp.oksocial.commands.SimpleCommand;
-
-public class Fbgraph extends SimpleCommand {
-  public Fbgraph() {
-    super("fbgraph", "https://graph.facebook.com", FacebookAuthInterceptor.NAME);
-  }
-}
