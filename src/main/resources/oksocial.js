b1537e5316bcb72c04b4299d224bcde4f627b084
Yuri Schimke <yuri@schimke.ee>
2016-05-17 20:56:34 -0700

    location support

--- src/main/resources/oksocial.js
+++ src/main/resources/oksocial.js
@@ -4,5 +4,11 @@ var okshell = Java.type("com.baulsupp.oksocial.jjs.OkShell").instance();
 okshell.listenForMainExit();
 
 var query = function(url) {
+  // TODO the response format to decide how to parse
   return JSON.parse(okshell.query(url));
+}
+
+var location = function() {
+  var a = Java.type("com.baulsupp.oksocial.location.Location");
+  return a.read().get();
 }
