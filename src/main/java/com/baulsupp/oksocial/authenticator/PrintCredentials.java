6f6cb625f688d0247c0b0c1b8a79172868af3a8d
Yuri Schimke <yuri@schimke.ee>
2016-05-19 22:12:32 -0700

    show credentials

---
+++ src/main/java/com/baulsupp/oksocial/authenticator/PrintCredentials.java
@@ -0,0 +1,19 @@
+package com.baulsupp.oksocial.authenticator;
+
+import com.baulsupp.oksocial.credentials.ServiceDefinition;
+
+public class PrintCredentials {
+  public static <T> void printKnownCredentials(AuthInterceptor<T> a) {
+    T credentials = a.credentials();
+    ServiceDefinition<T> sd = a.credentialsStore().getServiceDefinition();
+    String credentialsString =
+        credentials != null ? sd.formatCredentialsString(credentials) : "None";
+    System.out.println(sd.serviceName() + " " + credentialsString);
+  }
+
+  public static void printKnownCredentials(Iterable<AuthInterceptor<?>> services) {
+    for (AuthInterceptor a : services) {
+      PrintCredentials.printKnownCredentials(a);
+    }
+  }
+}
