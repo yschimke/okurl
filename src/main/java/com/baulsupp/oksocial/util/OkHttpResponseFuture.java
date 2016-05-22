0ec6681b7fde4305fe8905582790c6adafafe5ec
Yuri Schimke <yuri@schimke.ee>
2016-05-21 19:03:15 -0700

    request concurrency

---
+++ src/main/java/com/baulsupp/oksocial/util/OkHttpResponseFuture.java
@@ -0,0 +1,24 @@
+package com.baulsupp.oksocial.util;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class OkHttpResponseFuture implements Callback {
+  public final CompletableFuture<Response> future = new CompletableFuture<>();
+  public Call call;
+
+  public OkHttpResponseFuture(Call call) {
+    this.call = call;
+  }
+
+  @Override public void onFailure(Call call, IOException e) {
+    future.completeExceptionally(e);
+  }
+
+  @Override public void onResponse(Call call, Response response) throws IOException {
+    future.complete(response);
+  }
+}
