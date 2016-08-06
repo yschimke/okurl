59c928d1a3c9fefa2e3095230b423f7e21a20d50
Yuri Schimke <yuri@schimke.ee>
2016-08-06 07:28:48 +0100

    microsoft graph support

---
+++ src/main/java/com/baulsupp/oksocial/services/microsoft/MicrosoftUtil.java
@@ -0,0 +1,22 @@
+package com.baulsupp.oksocial.services.microsoft;
+
+import com.google.common.collect.Sets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import okhttp3.Request;
+
+public class MicrosoftUtil {
+  private MicrosoftUtil() {
+  }
+
+  public static final Set<String> API_HOSTS =
+      Collections.unmodifiableSet(Sets.newHashSet(
+          "graph.microsoft.com")
+      );
+
+  public static Request apiRequest(String s, Request.Builder requestBuilder) {
+    return requestBuilder.url("https://graph.microsoft.com" + s).build();
+  }
+}
