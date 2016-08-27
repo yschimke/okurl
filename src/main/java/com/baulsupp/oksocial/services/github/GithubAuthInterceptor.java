3bb6e7404b8ebfca6b30ad059c364bf4c1f39c25
Yuri Schimke <yschimke@fb.com>
2016-08-27 08:17:08 +0100

    Github support

---
+++ src/main/java/com/baulsupp/oksocial/services/github/GithubAuthInterceptor.java
@@ -0,0 +1,72 @@
+package com.baulsupp.oksocial.services.github;
+
+import com.baulsupp.oksocial.authenticator.AuthInterceptor;
+import com.baulsupp.oksocial.authenticator.AuthUtil;
+import com.baulsupp.oksocial.authenticator.JsonCredentialsValidator;
+import com.baulsupp.oksocial.authenticator.ValidatedCredentials;
+import com.baulsupp.oksocial.authenticator.oauth2.Oauth2ServiceDefinition;
+import com.baulsupp.oksocial.authenticator.oauth2.Oauth2Token;
+import com.baulsupp.oksocial.credentials.ServiceDefinition;
+import com.baulsupp.oksocial.output.OutputHandler;
+import com.baulsupp.oksocial.secrets.Secrets;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.Future;
+import okhttp3.Credentials;
+import okhttp3.Interceptor;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static com.baulsupp.oksocial.authenticator.JsonCredentialsValidator.fieldExtractor;
+
+/**
+ * https://developer.github.com/docs/authentication
+ */
+public class GithubAuthInterceptor implements AuthInterceptor<Oauth2Token> {
+  @Override public ServiceDefinition<Oauth2Token> serviceDefinition() {
+    return new Oauth2ServiceDefinition("api.github.com", "Github API", "github");
+  }
+
+  @Override public Response intercept(Interceptor.Chain chain, Oauth2Token credentials)
+      throws IOException {
+    Request request = chain.request();
+
+    String token = credentials.accessToken;
+
+    request =
+        request.newBuilder().addHeader("Authorization", "Bearer " + token).build();
+
+    return chain.proceed(request);
+  }
+
+  @Override public Oauth2Token authorize(OkHttpClient client, OutputHandler outputHandler,
+      List<String> authArguments) throws IOException {
+    System.err.println("Authorising Github API");
+
+    String clientId =
+        Secrets.prompt("Github Client Id", "github.clientId", "", false);
+    String clientSecret =
+        Secrets.prompt("Github Client Secret", "github.clientSecret", "", true);
+    Set<String> scopes =
+        Secrets.promptArray("Scopes", "github.scopes", GithubUtil.SCOPES);
+
+    return GithubAuthFlow.login(client, outputHandler, clientId, clientSecret, scopes);
+  }
+
+  @Override public Future<Optional<ValidatedCredentials>> validate(OkHttpClient client,
+      Request.Builder requestBuilder, Oauth2Token credentials) throws IOException {
+    return new JsonCredentialsValidator(
+        GithubUtil.apiRequest("/user", requestBuilder), fieldExtractor("name")).validate(
+        client);
+  }
+
+  @Override public Set<String> hosts() {
+    return GithubUtil.API_HOSTS;
+  }
+}
