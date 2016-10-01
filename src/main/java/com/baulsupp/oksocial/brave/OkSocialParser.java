a3cf3e4f0ce83d14fe276c6401daad1f49c295a1
Yuri Schimke <yschimke@fb.com>
2016-10-01 20:42:26 +0100

    more http annotations

--- src/main/java/com/baulsupp/oksocial/brave/OkSocialParser.java
+++ src/main/java/com/baulsupp/oksocial/brave/OkSocialParser.java
@@ -3,6 +3,7 @@ package com.baulsupp.oksocial.brave;
 import com.github.kristofa.brave.KeyValueAnnotation;
 import com.github.kristofa.brave.okhttp.OkHttpParser;
 import com.google.common.collect.Lists;
+import java.io.IOException;
 import java.util.List;
 import okhttp3.Handshake;
 import okhttp3.Headers;
@@ -16,8 +17,22 @@ public class OkSocialParser extends OkHttpParser {
     List<KeyValueAnnotation> tags = Lists.newArrayList();
 
     tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_URL, request.url().toString()));
+    tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_METHOD, request.method()));
+    tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_HOST, request.url().host()));
+    tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_PATH, request.url().encodedPath()));
+
+    //try {
+    //  long contentLength = request.body().contentLength();
+    //  if (contentLength > -1) {
+    //    tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_REQUEST_SIZE, "" + contentLength));
+    //  }
+    //} catch (IOException e) {
+    //  // TODO worth warning?
+    //}
 
     addHeaders(tags, request.headers(), "http.request.header");
+
+    System.out.println(tags);
 
     return tags;
   }
@@ -37,6 +52,11 @@ public class OkSocialParser extends OkHttpParser {
         KeyValueAnnotation.create(TraceKeys.HTTP_STATUS_CODE, String.valueOf(response.code())));
     tags.add(KeyValueAnnotation.create("http.protocol", response.protocol().toString()));
 
+    //long contentLength = response.body().contentLength();
+    //if (contentLength > -1) {
+    //  tags.add(KeyValueAnnotation.create(TraceKeys.HTTP_RESPONSE_SIZE, "" + contentLength));
+    //}
+
     Handshake handshake = response.handshake();
     if (handshake != null) {
       tags.add(KeyValueAnnotation.create("http.tls", handshake.tlsVersion().toString()));
@@ -45,6 +65,8 @@ public class OkSocialParser extends OkHttpParser {
     }
 
     addHeaders(tags, response.headers(), "http.response.header");
+
+    System.out.println(tags);
 
     return tags;
   }
