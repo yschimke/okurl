46374f72b31e41e26fc8ca09287614811e9be6db
Yuri Schimke <yuri@schimke.ee>
2016-11-05 16:56:39 +0000

    test completion

--- src/main/java/com/baulsupp/oksocial/services/transferwise/TransferwiseTestAuthInterceptor.java
+++ src/main/java/com/baulsupp/oksocial/services/transferwise/TransferwiseTestAuthInterceptor.java
@@ -1,6 +1,16 @@
 package com.baulsupp.oksocial.services.transferwise;
 
 import com.baulsupp.oksocial.authenticator.oauth2.Oauth2ServiceDefinition;
+import com.baulsupp.oksocial.completion.ApiCompleter;
+import com.baulsupp.oksocial.completion.BaseUrlCompleter;
+import com.baulsupp.oksocial.completion.CompletionVariableCache;
+import com.baulsupp.oksocial.completion.UrlList;
+import com.baulsupp.oksocial.credentials.CredentialsStore;
+import java.io.IOException;
+import java.util.List;
+import okhttp3.OkHttpClient;
+
+import static java.util.stream.Collectors.toList;
 
 public class TransferwiseTestAuthInterceptor extends TransferwiseAuthInterceptor {
   @Override protected String host() {
@@ -11,5 +21,16 @@ public class TransferwiseTestAuthInterceptor extends TransferwiseAuthInterceptor
     return new Oauth2ServiceDefinition(host(), "Transferwise Test API", "transferwise-test");
   }
 
+  @Override public ApiCompleter apiCompleter(String prefix, OkHttpClient client,
+      CredentialsStore credentialsStore, CompletionVariableCache completionVariableCache)
+      throws IOException {
+    UrlList urlList = UrlList.fromResource("transferwise").get();
 
+    List<String> testUrls = urlList.getUrls("")
+        .stream()
+        .map(s -> s.replace("api.transferwise.com", host()))
+        .collect(toList());
+
+    return new BaseUrlCompleter(new UrlList(UrlList.Match.SITE, testUrls), hosts());
+  }
 }
