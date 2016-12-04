aa661fd9d5006e1928c25995e9d3e9d50615f73c
Yuri Schimke <yuri@schimke.ee>
2016-12-04 10:53:57 +0000

    3.5

--- src/main/java/com/baulsupp/oksocial/ws/WebSocketPrinter.java
+++ src/main/java/com/baulsupp/oksocial/ws/WebSocketPrinter.java
@@ -1,35 +1,26 @@
 package com.baulsupp.oksocial.ws;
 
-import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
-import okhttp3.ws.WebSocket;
-import okhttp3.ws.WebSocketListener;
-import okio.Buffer;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
 
-public class WebSocketPrinter implements WebSocketListener {
+public class WebSocketPrinter extends WebSocketListener {
   private CountDownLatch latch = new CountDownLatch(1);
 
   public void waitForExit() throws InterruptedException {
     latch.await();
   }
 
-  @Override public void onOpen(WebSocket webSocket, Response response) {
+  @Override public void onMessage(WebSocket webSocket, String text) {
+    System.out.println(text);
   }
 
-  @Override public void onFailure(IOException e, Response response) {
-    e.printStackTrace();
-  }
-
-  @Override public void onMessage(ResponseBody responseBody) throws IOException {
-    System.out.println(responseBody.string());
-  }
-
-  @Override public void onPong(Buffer buffer) {
-  }
-
-  @Override public void onClose(int i, String s) {
+  @Override public void onClosed(WebSocket webSocket, int code, String reason) {
     latch.countDown();
+  }
+
+  @Override public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+    t.printStackTrace();
   }
 }
