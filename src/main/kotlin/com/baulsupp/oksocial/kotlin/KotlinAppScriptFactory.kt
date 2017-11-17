8f5c3be37373ed02f8db6b4ea26073e36e962e79
Yuri Schimke <yschimke@fb.com>
2017-11-17 12:00:40 +0800

    embedded

--- src/main/kotlin/com/baulsupp/oksocial/kotlin/KotlinAppScriptFactory.kt
+++ src/main/kotlin/com/baulsupp/oksocial/kotlin/KotlinAppScriptFactory.kt
@@ -1,11 +1,16 @@
 package com.baulsupp.oksocial.kotlin
 
-import com.intellij.openapi.diagnostic.Logger
-import org.apache.log4j.Level
+//import com.intellij.openapi.diagnostic.Logger
+//import org.apache.log4j.Level
 import org.jetbrains.kotlin.cli.common.repl.KotlinJsr223JvmScriptEngineFactoryBase
-import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngineFactory
+import org.jetbrains.kotlin.cli.common.repl.ScriptArgsWithTypes
+import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
+import org.jetbrains.kotlin.script.jsr223.KotlinJsr223JvmLocalScriptEngine
+import org.jetbrains.kotlin.script.jsr223.KotlinStandardJsr223ScriptTemplate
 import java.io.File
 import java.lang.management.ManagementFactory
+import javax.script.Bindings
+import javax.script.ScriptContext
 import javax.script.ScriptEngine
 
 
@@ -13,15 +18,15 @@ class KotlinAppScriptFactory : KotlinJsr223JvmScriptEngineFactoryBase() {
 
 
   override fun getScriptEngine(): ScriptEngine =
-      KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
-//      KotlinJsr223JvmLocalScriptEngine(
-//          Disposer.newDisposable(),
-//          this,
-//          classpath(),
-//          KotlinStandardJsr223ScriptTemplate::class.qualifiedName!!,
-//          { ctx, types -> ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), types ?: emptyArray()) },
-//          arrayOf(Bindings::class)
-//      )
+//      KotlinJsr223JvmLocalScriptEngineFactory().scriptEngine
+      KotlinJsr223JvmLocalScriptEngine(
+          Disposer.newDisposable(),
+          this,
+          classpath(),
+          KotlinStandardJsr223ScriptTemplate::class.qualifiedName!!,
+          { ctx, types -> ScriptArgsWithTypes(arrayOf(ctx.getBindings(ScriptContext.ENGINE_SCOPE)), types ?: emptyArray()) },
+          arrayOf(Bindings::class)
+      )
 
   private fun classpath(): List<File> {
     val classpathFromClassloader = ManagementFactory.getRuntimeMXBean().classPath.split(File.pathSeparator).map { File(it) }.toList()
