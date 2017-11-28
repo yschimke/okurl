f2cfff9c9673756a90e4922188aefdb34e360a66
Yuri Schimke <yuri@schimke.ee>
2017-11-28 22:49:45 +0000

    completion error handling (#287)

---
+++ src/main/kotlin/com/baulsupp/oksocial/services/facebook/XxX.kt
@@ -0,0 +1,10 @@
+package com.baulsupp.oksocial.services.facebook
+
+import com.baulsupp.oksocial.Main
+
+object XxX {
+  @JvmStatic
+  fun main(args: Array<String>) {
+    Main.main("https://graph.facebook.com/v2", "--urlCompletion")
+  }
+}
