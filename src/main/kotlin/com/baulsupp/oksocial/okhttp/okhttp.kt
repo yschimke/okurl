382191770246abcfbe5befa7342c6bba65af0ea6
Yuri Schimke <yuri@schimke.ee>
2017-12-08 18:43:04 +0000

    Cipher options (#290)
    * publish command
    
    * icipher options

---
+++ src/main/kotlin/com/baulsupp/oksocial/okhttp/okhttp.kt
@@ -0,0 +1,17 @@
+package com.baulsupp.oksocial.okhttp
+
+import okhttp3.CipherSuite
+import okhttp3.ConnectionSpec
+import okhttp3.TlsVersion
+
+enum class ConnectionSpecOption(val spec: ConnectionSpec) {
+  MODERN_TLS(ConnectionSpec.MODERN_TLS), COMPATIBLE_TLS(ConnectionSpec.COMPATIBLE_TLS), CLEARTEXT(ConnectionSpec.CLEARTEXT);
+}
+
+class CipherSuiteOption(s: String) {
+  val suite = CipherSuite.forJavaName(s)
+}
+
+class TlsVersionOption(s: String) {
+  val version = TlsVersion.forJavaName(s)
+}
