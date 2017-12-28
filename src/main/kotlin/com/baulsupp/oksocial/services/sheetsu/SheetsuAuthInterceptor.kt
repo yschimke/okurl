d33281e150d1f6c92811532434beb28efc61e193
Yuri Schimke <yuri@schimke.ee>
2017-12-28 05:22:24 +0000

    sheetsu moved back

---
+++ src/main/kotlin/com/baulsupp/oksocial/services/sheetsu/SheetsuAuthInterceptor.kt
@@ -0,0 +1,39 @@
+package com.baulsupp.oksocial.services.sheetsu
+
+import com.baulsupp.oksocial.authenticator.AuthInterceptor
+import com.baulsupp.oksocial.authenticator.BasicCredentials
+import com.baulsupp.oksocial.authenticator.basic.BasicAuthServiceDefinition
+import com.baulsupp.oksocial.output.OutputHandler
+import com.baulsupp.oksocial.secrets.Secrets
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import java.io.IOException
+
+class SheetsuAuthInterceptor : AuthInterceptor<BasicCredentials>() {
+  override fun serviceDefinition(): BasicAuthServiceDefinition =
+          BasicAuthServiceDefinition("sheetsu.com", "Sheetsu API", "sheetsu", "https://docs.sheetsu.com/", "https://sheetsu.com/dashboard")
+
+  @Throws(IOException::class) 
+  override fun intercept(chain: Interceptor.Chain, credentials: BasicCredentials): Response {
+    var request = chain.request()
+
+    request = request.newBuilder()
+            .addHeader("Authorization", Credentials.basic(credentials.user, credentials.password))
+            .build()
+
+    return chain.proceed(request)
+  }
+
+  override suspend fun authorize(client: OkHttpClient, outputHandler: OutputHandler<Response>, authArguments: List<String>): BasicCredentials {
+    val user =
+            Secrets.prompt("Sheetsu API Key", "sheetsu.apiKey", "", false)
+    val password =
+            Secrets.prompt("Sheetsu API Password", "sheetsu.apiSecret", "", true)
+
+    return BasicCredentials(user, password)
+  }
+
+  override fun hosts(): Set<String> = setOf("sheetsu.com")
+}
