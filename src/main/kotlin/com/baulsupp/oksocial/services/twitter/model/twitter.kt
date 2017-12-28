5697e2607532cb1e171720344490a8a403f03d26
Yuri Schimke <yuri@schimke.ee>
2017-12-28 06:56:00 +0000

    twitter model

---
+++ src/main/kotlin/com/baulsupp/oksocial/services/twitter/model/twitter.kt
@@ -0,0 +1,15 @@
+package com.baulsupp.oksocial.services.twitter.model
+
+data class User(val id_str: String, val name: String, val screen_name: String, val location: String, val description: String)
+
+data class Media(val id_str: String, val media_url_https: String, val display_url: String, val expanded_url: String, val type: String, val sizes: Map<String, Any> = mapOf())
+
+data class Entities(val hashtags: List<Any> = listOf(),
+                    val symbols: List<Any> = listOf(),
+                    val user_mentions: List<Any> = listOf(),
+                    val urls: List<Any> = listOf(),
+                    val media: List<Media> = listOf())
+
+data class Tweet(val id_str: String, val full_text: String, val user: User, val entities: Entities? = null)
+
+data class SearchResults(val statuses: List<Tweet>)
