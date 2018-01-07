e55bb24aade7ddebfba399fa75a7937235c40ab0
Yuri Schimke <yuri@schimke.ee>
2018-01-07 10:31:31 +0000

    explicit version of zipkin json format

--- src/main/kotlin/com/baulsupp/oksocial/tracing/HttpUriHandler.kt
+++ src/main/kotlin/com/baulsupp/oksocial/tracing/HttpUriHandler.kt
@@ -1,6 +1,7 @@
 package com.baulsupp.oksocial.tracing
 
 import zipkin2.Span
+import zipkin2.codec.SpanBytesEncoder
 import zipkin2.reporter.AsyncReporter
 import zipkin2.reporter.Reporter
 import zipkin2.reporter.okhttp3.OkHttpSender
@@ -8,7 +9,7 @@ import java.net.URI
 
 class HttpUriHandler : UriHandler {
   override fun buildSender(uri: URI): Reporter<Span>? = when {
-    uri.scheme == "http" || uri.scheme == "https" -> AsyncReporter.create(OkHttpSender.create(uri.toString()))
+    uri.scheme == "http" || uri.scheme == "https" -> AsyncReporter.builder(OkHttpSender.create(uri.toString())).build(SpanBytesEncoder.JSON_V2)
     else -> null
   }
 }
