c31482ead12890b761fbce0bcc6a4f50e327ce52
Yuri Schimke <yuri@schimke.ee>
2018-02-18 10:11:54 +0000

    Travis login

---
+++ src/main/kotlin/com/baulsupp/oksocial/process/Process.kt
@@ -0,0 +1,23 @@
+package com.baulsupp.oksocial.process
+
+import okio.ByteString
+import org.zeroturnaround.exec.ProcessExecutor
+import org.zeroturnaround.exec.stream.slf4j.Slf4jStream
+import java.nio.charset.StandardCharsets
+
+data class ExecResult(val exitCode: Int, val output: ByteString) {
+  val success = exitCode == 0
+  fun outputString(): String = output.string(StandardCharsets.UTF_8)
+}
+
+suspend fun exec(vararg command: String): ExecResult {
+  val pe = ProcessExecutor().command(command.toList())
+    .readOutput(true)
+    .redirectError(Slf4jStream.ofCaller().asInfo())
+
+  return run {
+    val pr = pe.execute()
+    val output = pr.output()
+    ExecResult(pr.exitValue, ByteString.of(output, 0, output.size))
+  }
+}
