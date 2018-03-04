55530cb09113c164f65ad448687509fe2c484417
Yuri Schimke <yuri@schimke.ee>
2018-03-04 09:26:52 +0000

    restricted mode

--- src/main/kotlin/com/baulsupp/oksocial/okhttp/okhttp.kt
+++ src/main/kotlin/com/baulsupp/oksocial/okhttp/okhttp.kt
@@ -7,10 +7,16 @@ import kotlin.reflect.full.staticProperties
 import kotlin.reflect.jvm.javaType
 
 val AllConnectionSpec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).allEnabledCipherSuites().allEnabledTlsVersions().build()
+val RestrictedConnectionSpec: ConnectionSpec = ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS).tlsVersions(TlsVersion.TLS_1_3, TlsVersion.TLS_1_2).cipherSuites(CipherSuite.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+  CipherSuite.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256,
+  CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+  CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+  CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+  CipherSuite.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384).build()
 
 enum class ConnectionSpecOption(val spec: ConnectionSpec) {
   ALL(AllConnectionSpec),
-  //  RESTRICTED_TLS(ConnectionSpec.RESTRICTED_TLS),
+  RESTRICTED_TLS(RestrictedConnectionSpec),
   MODERN_TLS(ConnectionSpec.MODERN_TLS),
   COMPATIBLE_TLS(ConnectionSpec.COMPATIBLE_TLS),
   CLEARTEXT(ConnectionSpec.CLEARTEXT);
