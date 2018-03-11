9dc74abbbcea498fb992d0a18404e9f320a01934
Yuri Schimke <yschimke@fb.com>
2018-03-11 18:59:26 +0000

    fetch images

--- src/test/kotlin/commands/tweetsearch.kts
+++ src/test/kotlin/commands/tweetsearch.kts
@@ -1,19 +1,37 @@
 #!/usr/bin/env okscript
 
-import com.baulsupp.oksocial.kotlin.*
-import com.baulsupp.oksocial.services.twitter.model.*
+import com.baulsupp.oksocial.kotlin.arguments
+import com.baulsupp.oksocial.kotlin.client
+import com.baulsupp.oksocial.kotlin.execute
+import com.baulsupp.oksocial.kotlin.query
+import com.baulsupp.oksocial.kotlin.request
+import com.baulsupp.oksocial.kotlin.show
+import com.baulsupp.oksocial.kotlin.showOutput
+import com.baulsupp.oksocial.services.twitter.model.SearchResults
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 import java.net.URLEncoder
 
 var argumentString = arguments.joinToString("+") { URLEncoder.encode(it, "UTF-8") };
 
-val a = query<SearchResults>(
-  "https://api.twitter.com/1.1/search/tweets.json?tweet_mode=extended&q=$argumentString")
+runBlocking {
+  val results = client.query<SearchResults>(
+    "https://api.twitter.com/1.1/search/tweets.json?tweet_mode=extended&q=$argumentString")
 
-a.statuses.forEach { tweet ->
-  println("%-20s: %s".format(tweet.user.screen_name, tweet.full_text))
+  val images = results.statuses.map {
+    it.id_str to it.entities?.media?.map {
+      async {
+        client.execute(request("${it.media_url_https}:thumb"))
+      }
+    }
+  }.toMap()
 
-  tweet.entities?.media?.firstOrNull()?.media_url_https?.let {
-    show("$it:thumb")
-    println()
+  results.statuses.forEach { tweet ->
+    println("%-20s: %s".format(tweet.user.screen_name, tweet.full_text))
+
+    images[tweet.id_str]?.forEach {
+      showOutput(it.await())
+      println()
+    }
   }
 }
