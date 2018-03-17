2b75bd2767121f7bb8e64a1f1b5a9cfd58d7b69c
Yuri Schimke <yuri@schimke.ee>
2018-03-17 12:21:53 +0000

    improve tracing information

--- src/main/kotlin/com/baulsupp/oksocial/tracing/ZipkinTracingListener.kt
+++ src/main/kotlin/com/baulsupp/oksocial/tracing/ZipkinTracingListener.kt
@@ -21,59 +21,59 @@ import java.util.function.Consumer
 class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, private val tracing: HttpTracing,
                             private val opener: Consumer<TraceContext>, private val detailed: Boolean) : EventListener() {
 
+  private lateinit var callSpan: Span
   private var connectSpan: Span? = null
   private var dnsSpan: Span? = null
-  private var callSpan: Span? = null
   private var spanInScope: Tracer.SpanInScope? = null
   private var requestSpan: Span? = null
   private var responseSpan: Span? = null
   private var secureConnectSpan: Span? = null
   private var connectionSpan: Span? = null
 
-  override fun callStart(call: Call?) {
+  override fun callStart(call: Call) {
     callSpan = tracer.newTrace().name("http").start()
 
-    if (!callSpan!!.isNoop) {
-      callSpan!!.tag(TraceKeys.HTTP_PATH, call!!.request().url().encodedPath())
-      callSpan!!.tag(TraceKeys.HTTP_METHOD, call.request().method())
-      callSpan!!.tag(TraceKeys.HTTP_HOST, call.request().url().host())
-      callSpan!!.kind(Span.Kind.CLIENT)
-    }
+    callSpan.tag(TraceKeys.HTTP_PATH, call.request().url().encodedPath())
+    callSpan.tag(TraceKeys.HTTP_METHOD, call.request().method())
+    callSpan.tag(TraceKeys.HTTP_HOST, call.request().url().host())
+    callSpan.tag(TraceKeys.HTTP_URL, call.request().url().toString())
+    callSpan.tag(TraceKeys.HTTP_ROUTE, "${call.request().method().toUpperCase()} ${call.request().url().encodedPath()}")
+    callSpan.kind(Span.Kind.CLIENT)
 
     spanInScope = tracer.withSpanInScope(callSpan)
   }
 
-  override fun callEnd(call: Call?) {
-    if (callSpan!!.isNoop) {
+  override fun callEnd(call: Call) {
+    if (callSpan.isNoop) {
       return
     }
 
     spanInScope!!.close()
-    callSpan!!.finish()
+    callSpan.finish()
 
-    opener.accept(callSpan!!.context())
+    opener.accept(callSpan.context())
   }
 
-  override fun callFailed(call: Call?, ioe: IOException?) {
-    if (callSpan!!.isNoop) {
+  override fun callFailed(call: Call, ioe: IOException?) {
+    if (callSpan.isNoop) {
       return
     }
 
-    callSpan!!.tag("error", ioe!!.toString())
+    callSpan.tag("error", ioe!!.toString())
 
     callEnd(call)
   }
 
-  override fun dnsStart(call: Call?, domainName: String?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun dnsStart(call: Call, domainName: String?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    dnsSpan = tracer.newChild(callSpan!!.context()).start().name("dns")
+    dnsSpan = tracer.newChild(callSpan.context()).start().name("dns")
   }
 
-  override fun dnsEnd(call: Call?, domainName: String?, inetAddressList: List<InetAddress>?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun dnsEnd(call: Call, domainName: String?, inetAddressList: List<InetAddress>?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
@@ -83,20 +83,20 @@ class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, 
     dnsSpan!!.finish()
   }
 
-  override fun connectStart(call: Call?, inetSocketAddress: InetSocketAddress?, proxy: Proxy?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun connectStart(call: Call, inetSocketAddress: InetSocketAddress?, proxy: Proxy?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    connectSpan = tracer.newChild(callSpan!!.context()).start().name("connect")
+    connectSpan = tracer.newChild(callSpan.context()).start().name("connect")
 
     connectSpan!!.tag("host", inetSocketAddress!!.toString())
     connectSpan!!.tag("proxy", proxy!!.toString())
   }
 
-  override fun connectEnd(call: Call?, inetSocketAddress: InetSocketAddress?, proxy: Proxy?,
+  override fun connectEnd(call: Call, inetSocketAddress: InetSocketAddress?, proxy: Proxy?,
                           protocol: Protocol?) {
-    if (callSpan!!.isNoop || !detailed) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
@@ -105,9 +105,9 @@ class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, 
     connectSpan!!.finish()
   }
 
-  override fun connectFailed(call: Call?, inetSocketAddress: InetSocketAddress?, proxy: Proxy?,
+  override fun connectFailed(call: Call, inetSocketAddress: InetSocketAddress?, proxy: Proxy?,
                              protocol: Protocol?, ioe: IOException?) {
-    if (callSpan!!.isNoop || !detailed) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
@@ -119,17 +119,18 @@ class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, 
     connectSpan!!.finish()
   }
 
-  override fun connectionAcquired(call: Call?, connection: Connection?) {
-    if (callSpan!!.isNoop) {
+  override fun connectionAcquired(call: Call, connection: Connection) {
+    if (callSpan.isNoop) {
       return
     }
 
-    connectionSpan = tracer.newChild(callSpan!!.context()).start().name("connection")
-    connectionSpan!!.tag("route", connection!!.route().socketAddress().toString())
+    val route = connection.route().socketAddress().toString()
+    connectionSpan = tracer.newChild(callSpan.context()).start().name("connection $route")
+    connectionSpan!!.tag("route", route)
   }
 
-  override fun connectionReleased(call: Call?, connection: Connection?) {
-    if (callSpan!!.isNoop) {
+  override fun connectionReleased(call: Call, connection: Connection?) {
+    if (callSpan.isNoop) {
       return
     }
 
@@ -148,44 +149,44 @@ class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, 
     connectionSpan!!.finish()
   }
 
-  override fun secureConnectStart(call: Call?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun secureConnectStart(call: Call) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    secureConnectSpan = tracer.newChild(callSpan!!.context()).start().name("tls")
+    secureConnectSpan = tracer.newChild(callSpan.context()).start().name("tls")
   }
 
-  override fun secureConnectEnd(call: Call?, handshake: Handshake?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun secureConnectEnd(call: Call, handshake: Handshake?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
     secureConnectSpan!!.finish()
   }
 
-  override fun requestHeadersStart(call: Call?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun requestHeadersStart(call: Call) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    requestSpan = tracer.newChild(callSpan!!.context()).start().name("request")
+    requestSpan = tracer.newChild(callSpan.context()).start().name("request")
   }
 
-  override fun requestHeadersEnd(call: Call?, request: Request?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun requestHeadersEnd(call: Call, request: Request?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
     requestSpan!!.tag("requestHeaderLength", "" + request!!.headers().byteCount())
   }
 
-  override fun requestBodyEnd(call: Call?, bytesWritten: Long) {
-    if (callSpan!!.isNoop) {
+  override fun requestBodyEnd(call: Call, bytesWritten: Long) {
+    if (callSpan.isNoop) {
       return
     }
 
-    requestSpan!!.tag("requestBodyBytes", "" + bytesWritten)
+    requestSpan!!.tag(TraceKeys.HTTP_REQUEST_SIZE, "" + bytesWritten)
 
     requestSpan = finish(requestSpan)
   }
@@ -195,34 +196,36 @@ class ZipkinTracingListener(private val call: Call, private val tracer: Tracer, 
     return null
   }
 
-  override fun responseHeadersStart(call: Call?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun responseHeadersStart(call: Call) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
     requestSpan = finish(requestSpan)
   }
 
-  override fun responseHeadersEnd(call: Call?, response: Response?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun responseHeadersEnd(call: Call, response: Response?) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    responseSpan = tracer.newChild(callSpan!!.context()).start().name("response").tag("responseHeaderLength", "" + response!!.headers().byteCount())
+    responseSpan = tracer.newChild(callSpan.context()).start()
+      .name("response")
+      .tag("responseHeaderLength", "" + response!!.headers().byteCount())
   }
 
-  override fun responseBodyEnd(call: Call?, bytesRead: Long) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun responseBodyEnd(call: Call, bytesRead: Long) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
 
-    responseSpan!!.tag("responseBodyBytes", "" + bytesRead)
+    responseSpan!!.tag(TraceKeys.HTTP_RESPONSE_SIZE, "" + bytesRead)
 
     responseSpan = finish(responseSpan)
   }
 
-  override fun responseBodyStart(call: Call?) {
-    if (callSpan!!.isNoop || !detailed) {
+  override fun responseBodyStart(call: Call) {
+    if (callSpan.isNoop || !detailed) {
       return
     }
   }
