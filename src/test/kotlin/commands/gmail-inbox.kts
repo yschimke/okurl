ff4ee4d3a97d74a4375bb59ae037cdb3c0d9b5d0
Yuri Schimke <yschimke@fb.com>
2018-05-07 08:46:21 +0100

    add gmail script

---
+++ src/test/kotlin/commands/gmail-inbox.kts
@@ -0,0 +1,14 @@
+#!/usr/bin/env okscript
+
+import com.baulsupp.oksocial.kotlin.*
+
+data class Thread(val id: String, val snippet: String, val historyId: String)
+data class ThreadList(val threads: List<Thread>, val nextPageToken: String?, val resultSizeEstimate: Int)
+
+val query = args.getOrElse(0, { _ -> "label:inbox" })
+
+val threads = query<ThreadList>("https://www.googleapis.com/gmail/v1/users/me/threads?q=$query")
+
+threads.threads.forEach {
+  println(it.snippet)
+}
