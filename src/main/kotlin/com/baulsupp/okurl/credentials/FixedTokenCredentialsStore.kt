661e875bf1e5bac057d85a5a286bfc701983d9dc
Yuri Schimke <yuri@schimke.ee>
2018-09-09 19:23:29 +0100

    swallow errors

--- src/main/kotlin/com/baulsupp/okurl/credentials/FixedTokenCredentialsStore.kt
+++ src/main/kotlin/com/baulsupp/okurl/credentials/FixedTokenCredentialsStore.kt
@@ -1,9 +1,17 @@
 package com.baulsupp.okurl.credentials
 
+import java.util.logging.Logger
+
 class FixedTokenCredentialsStore(private val token: String) : CredentialsStore {
+  private val logger = Logger.getLogger(FixedTokenCredentialsStore::class.java.name)
 
   override fun <T> get(serviceDefinition: ServiceDefinition<T>, tokenSet: String): T? {
-    return serviceDefinition.parseCredentialsString(token)
+    return try {
+      serviceDefinition.parseCredentialsString(token)
+    } catch (e: RuntimeException) {
+      // assume not for this one
+      null
+    }
   }
 
   override fun <T> set(
