8f4728145564e91948f4644013c20258a2636c1d
Yuri Schimke <yuri@schimke.ee>
2018-10-26 06:29:55 +0100

    test release

--- create_release.sh
+++ create_release.sh
@@ -7,16 +7,11 @@ git co master
 git pull
 
 VERSION="$1"
-TAG_VERSION="${VERSION}.1"
+TAG_VERSION="${VERSION}.0"
 BRANCH="release/$VERSION"
 
 git co -b "$BRANCH"
-
 git push origin "$BRANCH"
-
-git tag "$TAG_VERSION"
-
-git push origin "$TAG_VERSION"
 
 RELEASE_BODY=$(cat <<EOF
 {
@@ -35,6 +30,9 @@ RELEASE_ID=$(okurl -d "$RELEASE_BODY" https://api.github.com/repos/yschimke/okur
 echo Created "https://api.github.com/repos/yschimke/okurl/releases/${RELEASE_ID}"
 
 ./gradlew -q clean distTar bintrayUpload
+
+git tag "$TAG_VERSION"
+git push origin "$TAG_VERSION"
 
 ./okurl -H "Content-Type: application/x-gzip" -d "@build/distributions/okurl-${TAG_VERSION}.tgz" "https://uploads.github.com/repos/yschimke/okurl/releases/${RELEASE_ID}/assets?name=okurl-${TAG_VERSION}.tgz" | jq ".browser_download_url"
 
