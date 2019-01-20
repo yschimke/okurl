b4a66234ffc0ff8d91d8f7efe42686848bdd6c4f
Yuri Schimke <yuri@schimke.ee>
2019-01-20 10:47:42 +0000

    Fix

--- src/main/kotlin/com/baulsupp/okurl/services/strava/model/strava.kt
+++ src/main/kotlin/com/baulsupp/okurl/services/strava/model/strava.kt
@@ -6,19 +6,19 @@ data class Athlete(
   val firstname: String,
   val follower: Any? = null,
   val city: Any? = null,
-  val resource_state: Int,
-  val sex: String,
+  val resource_state: Int?,
+  val sex: String?,
   val profile: String? = null,
-  val created_at: String,
-  val summit: Boolean,
+  val created_at: String?,
+  val summit: Boolean?,
   val lastname: String,
-  val premium: Boolean,
+  val premium: Boolean?,
   val updated_at: String,
   val badge_type_id: Int,
   val friend: Any? = null,
   val id: Int,
   val state: Any? = null,
-  val email: String,
+  val email: String?,
   val username: String
 )
 
@@ -69,7 +69,7 @@ data class ActivitySummary(
   val achievement_count: Int,
   val suffer_score: Any? = null,
   val name: String,
-  val commute: Boolean
+  val commute: Boolean?
 )
 
 data class AthleteLink(
