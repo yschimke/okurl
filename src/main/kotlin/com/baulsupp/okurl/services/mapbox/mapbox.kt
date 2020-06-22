e6100e40b5f02b3ae7b195704db0b7dbbd05316a
Yuri Schimke <yuri@schimke.ee>
2020-06-22 20:53:41 +0100

    Fix mapbox

--- src/main/kotlin/com/baulsupp/okurl/services/mapbox/mapbox.kt
+++ src/main/kotlin/com/baulsupp/okurl/services/mapbox/mapbox.kt
@@ -4,6 +4,7 @@ import com.baulsupp.okurl.location.Location
 import java.net.URLEncoder
 
 class StaticMapBuilder {
+  var style: String = Dark
   val markers = mutableListOf<String>()
 
   fun pinLocation(loc: Location?, markerType: String? = "m-marker+CCC") {
@@ -18,12 +19,22 @@ class StaticMapBuilder {
 
   fun route(r: String?) {
     if (r != null) {
-      markers.add("path-2+f44-0.75+f44-0.0(${URLEncoder.encode(r, "UTF-8")})")
+      markers.add("path-6+f44-0.75+f44-0.0(${URLEncoder.encode(r, "UTF-8")})")
     }
+  }
+
+  companion object {
+    const val Light = "light-v10"
+    const val Dark = "dark-v10"
+    const val Streets = "streets-v11"
+    const val Outdoors = "outdoors-v11"
+    const val Satellite = "satellite-v9"
+    const val SatelliteStreets = "satellite-streets-v11"
   }
 }
 
 fun staticMap(init: StaticMapBuilder.() -> Unit = {}): String {
-  val markers = StaticMapBuilder().apply(init).markers
-  return "https://api.mapbox.com/v4/mapbox.dark/${markers.joinToString(",")}/auto/400x400.png"
+  val builder = StaticMapBuilder().apply(init)
+  val markers = builder.markers
+  return "https://api.mapbox.com/styles/v1/mapbox/${builder.style}/static/${markers.joinToString(",")}/auto/1024x1024"
 }
