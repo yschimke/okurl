e11918950404b831cd14c43b3cdf338a1d8cffb4
Yuri Schimke <yuri@schimke.ee>
2020-09-21 20:16:22 +0100

    Fix with graal

--- src/main/kotlin/com/baulsupp/okurl/services/mapbox/model/mapbox.kt
+++ src/main/kotlin/com/baulsupp/okurl/services/mapbox/model/mapbox.kt
@@ -3,10 +3,7 @@ package com.baulsupp.okurl.services.mapbox.model
 import com.baulsupp.okurl.location.Location
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
-import com.squareup.moshi.JsonReader
-import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.ToJson
 
 @JsonClass(generateAdapter = true)
