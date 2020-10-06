bff3d1ceee7876cab415c1a1fba486cfb64b3009
Yuri Schimke <yuri@schimke.ee>
2020-10-06 06:36:07 +0100

    Dependency Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
   application
   id("net.nemerosa.versioning") version "2.13.1"
   id("com.diffplug.spotless") version "5.1.0"
-  id("com.palantir.graal") version "0.7.1-20-g113a84d"
+  id("com.palantir.graal") version "0.7.1"
+  id("com.github.johnrengelman.shadow") version "6.0.0"
 }
 
 application {
@@ -94,16 +95,16 @@ dependencies {
     exclude(group = "com.squareup.okhttp3")
   }
   implementation("com.jakewharton.byteunits:byteunits:0.9.1")
-  implementation("com.squareup.moshi:moshi:1.10.0")
-  implementation("com.squareup.moshi:moshi-adapters:1.10.0")
-  implementation("com.squareup.moshi:moshi-kotlin:1.10.0")
+  implementation("com.squareup.moshi:moshi:1.11.0")
+  implementation("com.squareup.moshi:moshi-adapters:1.11.0")
+  implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
   implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
   implementation("com.squareup.okhttp3:okhttp:4.9.0")
   implementation("com.squareup.okhttp3:okhttp-brotli:4.9.0")
   implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:4.9.0")
   implementation("com.squareup.okhttp3:okhttp-sse:4.9.0")
   implementation("com.squareup.okhttp3:okhttp-tls:4.9.0")
-  implementation("com.squareup.okio:okio:2.8.0")
+  implementation("com.squareup.okio:okio:2.9.0")
   implementation("info.picocli:picocli:4.5.0")
   implementation("org.fusesource.jansi:jansi:1.18")
   implementation("io.jsonwebtoken:jjwt-api:0.10.6")
@@ -120,7 +121,7 @@ dependencies {
   implementation("io.zipkin.reporter2:zipkin-sender-okhttp3:2.10.2") {
     exclude(group = "com.squareup.okhttp3")
   }
-  implementation("org.conscrypt:conscrypt-openjdk-uber:2.5.0")
+  implementation("org.conscrypt:conscrypt-openjdk-uber:2.5.1")
   implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
   implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
@@ -145,9 +146,9 @@ dependencies {
 //    exclude(group= "org.graalvm.sdk")
     exclude(group= "org.graalvm.compiler")
   }
-  kapt("info.picocli:picocli-codegen:4.5.0")
+  kapt("info.picocli:picocli-codegen:4.5.1")
 
-  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.10.0")
+  kapt("com.squareup.moshi:moshi-kotlin-codegen:1.11.0")
   implementation("io.github.classgraph:classgraph:4.8.87")
 
   implementation("io.swagger.parser.v3:swagger-parser:2.0.21")
